9dc6aeeb861202cfa311a5f7db2fb13155d0c9c8
Roman Penyaev <rpenyaev@suse.de>
2020-02-20 20:38:59 +0100

    mempool: simplest mempool implementation ever possible

--- include/mempool.h
+++ include/mempool.h
@@ -3,21 +3,38 @@
 #define _MEMPOOL_H
 
 #include "types.h"
-
-struct mempool {};
-typedef struct mempool mempool_t;
+#include "slab.h"
 
 typedef void * (mempool_alloc_t)(gfp_t gfp_mask, void *pool_data);
 typedef void (mempool_free_t)(void *element, void *pool_data);
 
+struct mempool {
+	mempool_alloc_t *alloc;
+	mempool_free_t  *free;
+	void            *pool_data;
+	int             nr;
+};
+typedef struct mempool mempool_t;
+
 extern mempool_t *mempool_create(int min_nr, mempool_alloc_t *alloc_fn,
 			mempool_free_t *free_fn, void *pool_data);
-
 extern void mempool_destroy(mempool_t *pool);
 extern void *mempool_alloc(mempool_t *pool, gfp_t gfp_mask) __malloc;
 extern void mempool_free(void *element, mempool_t *pool);
 
-#define mempool_create_slab_pool(min, kc)		\
-	mempool_create(min, NULL, NULL, kc)
+/*
+ * A mempool_alloc_t and mempool_free_t that get the memory from
+ * a slab cache that is passed in through pool_data.
+ * Note: the slab cache may not have a ctor function.
+ */
+void *mempool_alloc_slab(gfp_t gfp_mask, void *pool_data);
+void mempool_free_slab(void *element, void *pool_data);
+
+static inline mempool_t *
+mempool_create_slab_pool(int min_nr, struct kmem_cache *kc)
+{
+	return mempool_create(min_nr, mempool_alloc_slab, mempool_free_slab,
+			      (void *) kc);
+}
 
 #endif
--- src/mempool.c
+++ src/mempool.c
@@ -1,24 +1,55 @@
 #include "mempool.h"
 
+/*
+ * Keep it simple for now.
+ */
+
 mempool_t *mempool_create(int min_nr, mempool_alloc_t *alloc_fn,
 			  mempool_free_t *free_fn, void *pool_data)
 {
-	//XXX
-	return NULL;
+	mempool_t *pool;
+
+	pool = calloc(1, sizeof(*pool));
+	if (unlikely(!pool))
+		return NULL;
+
+	pool->nr        = 0;
+	pool->alloc     = alloc_fn;
+	pool->free      = free_fn;
+	pool->pool_data = pool_data;
+
+	return pool;
 }
 
 void mempool_destroy(mempool_t *pool)
 {
-	//XXX
+	WARN_ON(pool->nr);
+	free(pool);
 }
 
 void *mempool_alloc(mempool_t *pool, gfp_t gfp_mask)
 {
-	//XXX
-	return NULL;
+	pool->nr++;
+	return pool->alloc(gfp_mask, pool->pool_data);
 }
 
 void mempool_free(void *element, mempool_t *pool)
 {
-	//XXX
+	pool->free(element, pool->pool_data);
+	pool->nr--;
+}
+
+/*
+ * A commonly used alloc and free fn.
+ */
+void *mempool_alloc_slab(gfp_t gfp_mask, void *pool_data)
+{
+	struct kmem_cache *mem = pool_data;
+	return kmem_cache_alloc(mem, gfp_mask);
+}
+
+void mempool_free_slab(void *element, void *pool_data)
+{
+	struct kmem_cache *mem = pool_data;
+	kmem_cache_free(mem, element);
 }
