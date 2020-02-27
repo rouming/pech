5bada4e6abe2ba247ce70d62b4580eb702b63d5e
Roman Penyaev <rpenyaev@suse.de>
2020-02-27 16:21:47 +0100

    mutex,rwsem: validate incorrect locks/unlocks

--- include/mutex.h
+++ include/mutex.h
@@ -2,11 +2,35 @@
 #ifndef _MUTEX_H
 #define _MUTEX_H
 
-struct mutex {};
+/*
+ * Actually does absolutely nothing, but validates correctness
+ */
 
-#define mutex_init(m)
-#define mutex_lock(m)
-#define mutex_unlock(m)
-#define mutex_is_locked(m) (0)
+struct mutex {
+	int locked;
+};
+
+static inline void mutex_init(struct mutex *mutex)
+{
+	mutex->locked = 0;
+}
+
+static inline void mutex_lock(struct mutex *mutex)
+{
+	/* In our UP non-preemtible environment locked should not be observed */
+	WARN_ON(mutex->locked);
+	mutex->locked = 1;
+}
+
+static inline void mutex_unlock(struct mutex *mutex)
+{
+	WARN_ON(mutex->locked != 1);
+	mutex->locked = 0;
+}
+
+static inline bool mutex_is_locked(struct mutex *mutex)
+{
+	return mutex->locked;
+}
 
 #endif
--- include/rwsem.h
+++ include/rwsem.h
@@ -2,15 +2,61 @@
 #ifndef _RWSEM_H
 #define _RWSEM_H
 
-struct rw_semaphore {};
+#include "types.h"
+#include "bug.h"
 
-#define init_rwsem(sem) ((void)sem)
-#define up_read(sem) ((void)sem)
-#define down_read(sem) ((void)sem)
-#define down_write(sem) ((void)sem)
-#define up_write(sem) ((void)sem)
-#define rwsem_is_locked(sem) (0)
-#define downgrade_write(sem) ((void)sem)
-#define down_read_trylock(...) (1)
+/*
+ * Actually does absolutely nothing, but validates correctness
+ */
+
+struct rw_semaphore {
+	int locked;
+};
+
+static inline void init_rwsem(struct rw_semaphore *rwsem)
+{
+	rwsem->locked = 0;
+}
+
+static inline void up_read(struct rw_semaphore *rwsem)
+{
+	WARN_ON(!rwsem->locked);
+	rwsem->locked--;
+}
+
+static inline void down_read(struct rw_semaphore *rwsem)
+{
+	rwsem->locked++;
+}
+
+static inline void up_write(struct rw_semaphore *rwsem)
+{
+	WARN_ON(rwsem->locked != 1);
+	rwsem->locked = 0;
+}
+
+static inline void down_write(struct rw_semaphore *rwsem)
+{
+	/* In our UP non-preemtible environment locked should not be observed */
+	WARN_ON(rwsem->locked);
+	rwsem->locked = 1;
+}
+
+static inline bool rwsem_is_locked(struct rw_semaphore *rwsem)
+{
+	return rwsem->locked;
+}
+
+static inline void downgrade_write(struct rw_semaphore *rwsem)
+{
+	WARN_ON(rwsem->locked != 1);
+}
+
+static inline bool down_read_trylock(struct rw_semaphore *rwsem)
+{
+	down_read(rwsem);
+	/* In our UP non-preemtible environment always succeed */
+	return true;
+}
 
 #endif
