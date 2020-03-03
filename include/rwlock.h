0bcf212de3c6b5f348b0d08e08d437f3fc56a427
Roman Penyaev <rpenyaev@suse.de>
2020-03-03 11:43:16 +0100

    rwlock: add empty wrappers for write/read_lock/unlock_bh() calls

---
+++ include/rwlock.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _RWLOCK_H
+#define _RWLOCK_H
+
+#define write_lock_bh(...)
+#define write_unlock_bh(...)
+
+#define read_lock_bh(...)
+#define read_unlock_bh(...)
+
+#endif
