96d69f29b14f0e08860afd2cc8fcb51fb1236100
Roman Penyaev <rpenyaev@suse.de>
2020-04-06 14:35:50 +0200

    pagelist: implement set of encoding and copy helpers

--- include/ceph/pagelist.h
+++ include/ceph/pagelist.h
@@ -6,6 +6,7 @@
 #include "refcount.h"
 #include "list.h"
 #include "types.h"
+#include "messenger.h"
 
 struct ceph_pagelist {
 	struct list_head head;
@@ -23,9 +24,21 @@ extern void ceph_pagelist_release(struct ceph_pagelist *pl);
 
 extern int ceph_pagelist_append(struct ceph_pagelist *pl, const void *d, size_t l);
 
+extern int ceph_pagelist_copy_from_page(struct ceph_pagelist *pl,
+					struct page *page, size_t len);
+
 extern int ceph_pagelist_reserve(struct ceph_pagelist *pl, size_t space);
 
+extern int ceph_pagelist_truncate(struct ceph_pagelist *pl, size_t length);
+
 extern int ceph_pagelist_free_reserve(struct ceph_pagelist *pl);
+
+extern int ceph_pagelist_copy_from_buffer(struct ceph_pagelist *pl,
+					  const void *buf, size_t length,
+					  off_t off);
+extern int ceph_pagelist_copy_from_cursor(struct ceph_pagelist *pl,
+					  struct ceph_msg_data_cursor *cur,
+					  size_t len);
 
 static inline int ceph_pagelist_encode_64(struct ceph_pagelist *pl, u64 v)
 {
@@ -46,6 +59,12 @@ static inline int ceph_pagelist_encode_8(struct ceph_pagelist *pl, u8 v)
 {
 	return ceph_pagelist_append(pl, &v, 1);
 }
+static inline int ceph_pagelist_encode_32_at_offset(struct ceph_pagelist *pl,
+						    u32 v, off_t off)
+{
+	__le32 ev = cpu_to_le32(v);
+	return ceph_pagelist_copy_from_buffer(pl, &ev, sizeof(ev), off);
+}
 static inline int ceph_pagelist_encode_string(struct ceph_pagelist *pl,
 					      char *s, u32 len)
 {
@@ -54,6 +73,28 @@ static inline int ceph_pagelist_encode_string(struct ceph_pagelist *pl,
 		return ret;
 	if (len)
 		return ceph_pagelist_append(pl, s, len);
+	return 0;
+}
+static inline int ceph_pagelist_encode_pagelist(struct ceph_pagelist *pl,
+						const struct ceph_pagelist *src,
+						bool encode_length)
+{
+	size_t length = src->length;
+	struct page *page;
+	int ret;
+
+	if (encode_length) {
+		ret = ceph_pagelist_encode_32(pl, length);
+		if (ret)
+			return ret;
+	}
+	list_for_each_entry(page, &src->head, lru) {
+		ret = ceph_pagelist_copy_from_page(pl, page,
+					length & ~PAGE_MASK);
+		if (ret)
+			return ret;
+		length -= length & ~PAGE_MASK;
+	}
 	return 0;
 }
 
--- src/ceph/pagelist.c
+++ src/ceph/pagelist.c
@@ -95,6 +95,20 @@ int ceph_pagelist_append(struct ceph_pagelist *pl, const void *buf, size_t len)
 }
 EXPORT_SYMBOL(ceph_pagelist_append);
 
+int ceph_pagelist_copy_from_page(struct ceph_pagelist *pl, struct page *page,
+				 size_t len)
+{
+	void *addr;
+	int ret;
+
+	addr = kmap_atomic(page);
+	ret = ceph_pagelist_append(pl, addr, len);
+	kunmap_atomic(addr);
+
+	return ret;
+}
+EXPORT_SYMBOL(ceph_pagelist_copy_from_page);
+
 /* Allocate enough pages for a pagelist to append the given amount
  * of data without without allocating.
  * Returns: 0 on success, -ENOMEM on error.
@@ -117,6 +131,49 @@ int ceph_pagelist_reserve(struct ceph_pagelist *pl, size_t space)
 }
 EXPORT_SYMBOL(ceph_pagelist_reserve);
 
+int ceph_pagelist_truncate(struct ceph_pagelist *pl, size_t length)
+{
+	struct page *page, *tmp;
+	unsigned int n_page, last_page;
+
+	if (length > pl->length)
+		/* Do not expand, only truncate */
+		return -EINVAL;
+
+	if (length == pl->length)
+		/* Nothing to do */
+		return 0;
+
+	if (PAGE_ALIGN(length) == PAGE_ALIGN(pl->length)) {
+		/* Same page */
+		goto end;
+	}
+
+	ceph_pagelist_unmap_tail(pl);
+
+	n_page = 0;
+	last_page = ALIGN_DOWN(length, PAGE_SIZE) >> PAGE_SHIFT;
+	list_for_each_entry_safe(page, tmp, &pl->head, lru) {
+		if (n_page++ < last_page)
+			continue;
+
+		if (!pl->mapped_tail) {
+			/* Last page */
+			pl->mapped_tail = kmap(page);
+		} else {
+			/* Move everything to free list next after last page */
+			list_move(&page->lru, &pl->free_list);
+			++pl->num_pages_free;
+		}
+	}
+end:
+	pl->length = length;
+	pl->room = PAGE_SIZE - (pl->length & ~PAGE_MASK);
+
+	return 0;
+}
+EXPORT_SYMBOL(ceph_pagelist_truncate);
+
 /* Free any pages that have been preallocated. */
 int ceph_pagelist_free_reserve(struct ceph_pagelist *pl)
 {
@@ -131,3 +188,116 @@ int ceph_pagelist_free_reserve(struct ceph_pagelist *pl)
 	return 0;
 }
 EXPORT_SYMBOL(ceph_pagelist_free_reserve);
+
+int ceph_pagelist_copy_from_buffer(struct ceph_pagelist *pl, const void *buf,
+				   size_t length, off_t off_inpg)
+{
+	struct page *page;
+	size_t to_append;
+	off_t off_inbuf;
+	void *addr;
+	int ret;
+
+	if (pl->length < off_inpg)
+		/* Do not handle gaps */
+		return -EINVAL;
+
+	to_append = 0;
+	if (pl->length < off_inpg + length) {
+		to_append = (off_inpg + length) - pl->length;
+		ret = ceph_pagelist_reserve(pl, to_append);
+		if (ret)
+			return ret;
+	}
+
+	/* Limit to what we need to copy */
+	length = min(length, pl->length - off_inpg);
+
+	off_inbuf = 0;
+	list_for_each_entry(page, &pl->head, lru) {
+		size_t len;
+
+		if (!length)
+			break;
+
+		if (off_inpg >= PAGE_SIZE) {
+			off_inpg -= PAGE_SIZE;
+			continue;
+		}
+
+		len = min(length, PAGE_SIZE - off_inpg);
+		addr = kmap_atomic(page);
+		memcpy(addr + off_inpg, buf + off_inbuf, len);
+		kunmap_atomic(addr);
+
+		length -= len;
+		off_inbuf += len;
+		off_inpg = 0;
+	}
+
+	if (to_append) {
+		ret = ceph_pagelist_append(pl, buf + off_inbuf, to_append);
+		/* Memory should be reserved already, no errors expected */
+		WARN_ON(ret);
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL(ceph_pagelist_copy_from_buffer);
+
+int ceph_pagelist_copy_from_cursor(struct ceph_pagelist *pl,
+				   struct ceph_msg_data_cursor *cursor,
+				   size_t length)
+{
+	struct page *page;
+	size_t to_copy;
+	void *addr;
+	int ret;
+
+	if (pl->length < length) {
+		size_t to_append = length - pl->length;
+		ret = ceph_pagelist_reserve(pl, to_append);
+		if (ret)
+			return ret;
+	}
+
+	to_copy = length;
+	list_for_each_entry(page, &pl->head, lru) {
+		size_t len;
+
+		if (!to_copy)
+			break;
+
+		len = min(to_copy, PAGE_SIZE);
+		addr = kmap_atomic(page);
+		ret = ceph_msg_data_cursor_copy(cursor, addr, len);
+		kunmap_atomic(addr);
+		if (ret)
+			goto end;
+
+		to_copy -= len;
+	}
+
+	if (to_copy) {
+		size_t len;
+
+		ret = ceph_pagelist_addpage(pl);
+		/* Memory should be reserved already, no errors expected */
+		WARN_ON(ret);
+
+		len = min(to_copy, PAGE_SIZE);
+		ret = ceph_msg_data_cursor_copy(cursor, pl->mapped_tail, len);
+		if (ret)
+			goto end;
+
+		to_copy -= len;
+	}
+
+end:
+	pl->length = max(pl->length, length - to_copy);
+	pl->room = PAGE_SIZE - (pl->length & ~PAGE_MASK);
+
+	return ret;
+
+}
+EXPORT_SYMBOL(ceph_pagelist_copy_from_cursor);
